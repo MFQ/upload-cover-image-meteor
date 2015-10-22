c30872fb299befb265f0ed1a35d0c56256a3b297
MFQ <softmfq@gmail.com>
2015-10-22 16:14:12 +0500

    updated readme

---
+++ README.md
@@ -0,0 +1,10 @@
+
+### Install Account System
+    $ meteor add accounts-base
+    $ meteor add accounts-password
+
+### Install our Avatar-Upload packages
+    $ meteor add mfq:upload-cover-image
+
+### Create template and install on the application
+    {{> editCoverImage}}
--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'mfq:upload-cover-image',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'Simply upload cover image package for meteor',
+  summary: 'Upload Facebook like cover image and adjust its position package for meteor',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/MFQ/upload-cover-image-meteor',
   // By default, Meteor will default to using README.md for documentation.
