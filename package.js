f6c4aeac10aae5a1e0eec1abf98b1d714bdd4589
MFQ <softmfq@gmail.com>
2015-10-28 17:56:50 +0500

    version is updated 0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mfq:upload-cover-image',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Upload Facebook like cover image and adjust its position, package for meteor',
   // URL to the Git repository containing the source code for this package.
